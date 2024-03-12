fcb88c55d66aefe56a77b43172ac44044d4b456d
ajugalushkin <ajugalushkin@gmail.com>
2024-03-12 22:06:55 +0300

    Completed iter4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/ajugalushkin/url-shortener-version2/internal/app"
+import (
+	"github.com/ajugalushkin/url-shortener-version2/internal/app"
+	"github.com/ajugalushkin/url-shortener-version2/internal/config"
+)
 
 func main() {
+	config.ParseFlags()
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
