bbced31f4a8d12ac4cb397a0b83759b63e6f0b45
ajugalushkin <ajugalushkin@gmail.com>
2024-05-03 09:18:51 +0300

    iter15
    FIX: Fixed PR checks error

--- internal/handler/delete.go
+++ internal/handler/delete.go
@@ -8,7 +8,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"io"
 	"net/http"
-	"sync"
 )
 
 var userID int
@@ -42,14 +41,13 @@ func (s Handler) HandleUserUrlsDelete(echoCtx echo.Context) error {
 	doneCh := make(chan struct{})
 	defer close(doneCh)
 
-	inputCh := s.generator(doneCh, URLs)
-	channels := s.fanOut(doneCh, inputCh)
-	s.fanIn(doneCh, channels...)
+	inputCh := generator(doneCh, URLs)
+	s.deleteURL(inputCh)
 
 	return validate.AddMessageOK(s.ctx, echoCtx, "", http.StatusAccepted, 0)
 }
 
-func (s Handler) generator(doneCh chan struct{}, input []string) chan string {
+func generator(doneCh chan struct{}, input []string) chan string {
 	inputCh := make(chan string)
 
 	go func() {
@@ -67,70 +65,12 @@ func (s Handler) generator(doneCh chan struct{}, input []string) chan string {
 	return inputCh
 }
 
-func (s Handler) fanOut(doneCh chan struct{}, inputCh chan string) []chan string {
-	numWorkers := 10
-	channels := make([]chan string, numWorkers)
-
-	for i := 0; i < numWorkers; i++ {
-		addResultCh := s.deleteURL(doneCh, inputCh)
-		channels[i] = addResultCh
-	}
-	return channels
-}
-
-func (s Handler) fanIn(doneCh chan struct{}, resultChs ...chan string) {
-	finalCh := make(chan string)
-
-	var wg sync.WaitGroup
-
-	for _, ch := range resultChs {
-		chClosure := ch
-
-		wg.Add(1)
-
-		go func() {
-			defer wg.Done()
-
-			for data := range chClosure {
-				select {
-				case <-doneCh:
-					return
-				case finalCh <- data:
-				}
-			}
-		}()
-	}
-
-	go func() {
-		wg.Wait()
-		close(finalCh)
-	}()
-
+func (s Handler) deleteURL(inputCh chan string) {
 	log := logger.LogFromContext(s.ctx)
-	for res := range finalCh {
-		log.Info(res)
-	}
-}
-
-func (s Handler) deleteURL(doneCh chan struct{}, inputCh chan string) chan string {
-	addRes := make(chan string)
-
-	go func() {
-		defer close(addRes)
-
-		for data := range inputCh {
-			err := s.servAPI.DeleteUserURL(s.ctx, data, userID)
-			var result = "DeleteUserURL OK"
-			if err != nil {
-				result = err.Error()
-			}
-
-			select {
-			case <-doneCh:
-				return
-			case addRes <- result:
-			}
+	for URL := range inputCh {
+		err := s.servAPI.DeleteUserURL(s.ctx, URL, userID)
+		if err != nil {
+			log.Error(err.Error())
 		}
-	}()
-	return addRes
+	}
 }
