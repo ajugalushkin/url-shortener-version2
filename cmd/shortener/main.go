8072923d4a82e9fb011fc4cb18cf05a506a7efa1
ajugalushkin <ajugalushkin@gmail.com>
2024-07-13 21:05:27 +0300

    feat: add global var

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,5 @@
+//go:build windows || linux
+
 package main
 
 import (
@@ -6,6 +8,12 @@ import (
 
 	"github.com/ajugalushkin/url-shortener-version2/config"
 	"github.com/ajugalushkin/url-shortener-version2/internal/app"
+)
+
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
 )
 
 // @title shortener-url API
@@ -18,6 +26,10 @@ import (
 func main() {
 	cfg := config.ReadConfig()
 	ctx := config.ContextWithFlags(context.Background(), cfg)
+
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
 
 	if err := app.Run(ctx); err != nil {
 		fmt.Println(err)
