58d34ffac94ced0e1f5af17820e0662a0810f2e8
ajugalushkin <ajugalushkin@gmail.com>
2024-08-11 14:10:16 +0300

    feat: add tests

---
+++ internal/storage/switch_test.go
@@ -0,0 +1,36 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/ajugalushkin/url-shortener-version2/config"
+	"github.com/ajugalushkin/url-shortener-version2/internal/storage/file"
+	"github.com/ajugalushkin/url-shortener-version2/internal/storage/inmemory"
+)
+
+// Returns a file storage when FileStoragePath is provided
+func TestGetStorageReturnsFileStorage(t *testing.T) {
+	// Arrange
+	config.GetConfig().FileStoragePath = "mock_path"
+
+	// Act
+	storage := GetStorage()
+
+	// Assert
+	if _, ok := storage.(*file.Storage); !ok {
+		t.Errorf("expected *file.Storage, got %T", storage)
+	}
+}
+
+// Returns an in-memory storage when neither DataBaseDsn nor FileStoragePath is provided
+func TestGetStorageReturnsInMemory(t *testing.T) {
+	// Arrange
+
+	// Act
+	storage := GetStorage()
+
+	// Assert
+	if _, ok := storage.(*inmemory.InMemory); !ok {
+		t.Errorf("expected *inmemory.InMemory, got %T", storage)
+	}
+}
